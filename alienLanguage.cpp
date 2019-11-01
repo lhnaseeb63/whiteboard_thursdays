35b619ecf81e482f8298186ad4bd4b784e2124fe
lhnaseeb63 <57266049+lhnaseeb63@users.noreply.github.com>
2019-11-01 12:51:59 -0400

    Initial commit
    - problem and nothing else

---
+++ alienLanguage.cpp
@@ -0,0 +1,16 @@
+/*
+    It's late at night. You hear a rumbling in the sky. 
+    Suddenly a large crashing sound comes from your
+    backyard. You leap from your bed and go outside to 
+    investigate. Smoke is coming from behind your shed,
+    and suddenly you see a set of eyes peering from the 
+    darkness. An alien emerges. 
+    Although he seems friendly, he speaks a strange language.
+    It seems that every word he speaks consists 
+    of the same sequence of letters forwards as it 
+    does backwards. 
+    To begin communicating with this strange creature, 
+    you decide to write a program to determine if 
+    a word can or cannot be interpreted by the alien.
+    
+*/
